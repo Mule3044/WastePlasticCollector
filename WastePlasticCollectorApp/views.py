d2de1179164b3c668cc94a30912274bf2fa01aac
Mulugeta Molla <muler3044@gmail.com>
2024-06-29 22:49:10 +0300

    fix task assigned filter bug

--- WastePlasticCollectorApp/views.py
+++ WastePlasticCollectorApp/views.py
@@ -777,7 +777,7 @@ class TaskAssignedListAPIView(generics.ListAPIView):
     def get_queryset(self):
         try:
             requestor_id = self.kwargs.get('userId_id')
-            queryset = TaskAssigned.objects.filter(userId_id=id, userId_id__role="agent")
+            queryset = TaskAssigned.objects.filter(userId_id=user_id, userId__role="agent")
             return queryset
         except Exception as e:
             return TaskAssigned.objects.none()
