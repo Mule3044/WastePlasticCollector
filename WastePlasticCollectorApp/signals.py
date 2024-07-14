4519d3607cb5424bc7c65022428fd90a8efa6bf2
Mulugeta Molla <muler3044@gmail.com>
2024-07-14 23:27:15 +0300

    fix task assign userId bug

--- WastePlasticCollectorApp/signals.py
+++ WastePlasticCollectorApp/signals.py
@@ -48,7 +48,7 @@ def waste_plastic_requestor_created(sender, instance, created, **kwargs):
             # Create a TaskAssigned record
             task_assigned = TaskAssigned.objects.create(
                 requestId=instance,
-                userId=instance.requestor__id,
+                userId=instance.requestor.customusers_id,
                 asign_status='asign',
                 task_status='on progress',
                 assigned_date=timezone.now().date(),
